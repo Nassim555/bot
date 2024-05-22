68ef38fbb2a031fecb85ac3ff2649fdeec1625ea
Nassim 555 <48863215+Nassim555@users.noreply.github.com>
2024-05-22 14:12:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const { Client } = require("discord.js-selfbot-v13");
 const { joinVoiceChannel } = require("@discordjs/voice");
 
-const channel_id = process.env.id;
-
+const info = JSON.parse(process.env.token);
+const channel_id = info.id;
 function joinVc(client) {
   client.on("ready", () => {
     const channel = client.channels.cache.get(channel_id);
@@ -19,4 +19,4 @@ function joinVc(client) {
 
 const client = new Client({ checkUpdate: false });
 joinVc(client);
-client.login(process.env.token);
+client.login(info.token);
