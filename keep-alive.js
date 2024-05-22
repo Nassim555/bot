ca6f170c20780bfbfd0eaed775902152023262f9
Nassim 555 <48863215+Nassim555@users.noreply.github.com>
2024-05-22 17:33:52 +0100

    Update keep-alive.js

--- keep-alive.js
+++ keep-alive.js
@@ -6,9 +6,5 @@ const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 app.get('/', (req, res) => {
-  res.send(`
-    <body>
-      <h1>bot is alive</h1>
-    </body>
-  `);
+  res.send("bot is alive");
 });
